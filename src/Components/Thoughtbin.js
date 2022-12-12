a60aac2dd22d8c3e8f24b4e4d6eef91b8ec080af
simrellaagrawal <simsim44144@gmail.com>
2022-12-12 14:46:17 +0530

    responsive

--- src/Components/Thoughtbin.js
+++ src/Components/Thoughtbin.js
@@ -7,56 +7,48 @@ const Thoughtbin = () => {
   return (
     <>
       <div className="flex flex-row">
-        {/* <img className=" h-screen w-[400px]" src={Rectangle}  /> */}
-
-        <div className="bg-[#317B7B] w-[400px] h-[1000px]">
+        <div className="bg-[#317B7B] mobileMd:w-[120px] mobileMd:h-screen tablet:w-[280px] laptopMd:h-[850px] laptopMd:w-[300px] desktopMd:w-[400px] desktopMd:h-[900px]">
           <div className="absolute top-0">
-            <img className="w-[300px]" src={cloud} />
+            <img className="mobileMd:w-[120px] tablet:w-[205px] laptopMd:w-[230px] desktopMd:w-[300px]" src={cloud} />
           </div>
-          <div className="absolute top-1 left-10 ">
-            <img className="h-48" src={trash} />
+          <div className="absolute top-1 desktopMd:left-10 mobileMd:left-3 laptopMd:left-4 ">
+            <img className="mobileMd:h-20 tablet:h-[150px] laptopMd:h-[170px] desktopMd:h-48" src={trash} />
           </div>
-          <div className="mt-[404px] ">
-          <nav className="flex flex-col justify-center items-center">
+          <div className="desktopMd:mt-[404px] mobileMd:mt-[150px] tablet:mt-[250px] laptopMd:mt-[300px]">
+          <nav className="flex flex-col justify-center items-center mobileMd:pl-2">
           <div className="absolute ">
             <a
               href="#"
-              className=" font-['DM Sans'] text-white text-2xl not-italic leading-[57px] font-bold"
-            >
-              Home
-            </a>
+              className=" mobileMd:text-xs tablet:text-lg font-['DM Sans'] text-white desktopMd:text-2xl not-italic leading-[57px] font-bold"
+            >Home</a>
           </div>
-          <div className="absolute pt-36">
+          <div className="absolute mobileMd:pt-14 tablet:pt-24 laptopMd:pt-[100px] desktopMd:pt-36">
             <a
               href="#"
-              className=" font-['DM Sans'] text-white text-2xl not-italic leading-[57px] font-bold"
-            >
-              Team
-            </a>
+              className="mobileMd:text-xs  tablet:text-lg font-['DM Sans'] text-white desktopMd:text-2xl not-italic leading-[57px] font-bold"
+            >Team </a>
           </div>
-          <div className="absolute pt-72">
+          <div className="absolute mobileMd:pt-28 tablet:pt-52 desktopMd:pt-72">
             <a
               href="#"
-              className=" font-['DM Sans'] text-white text-2xl not-italic leading-[57px] font-bold"
-            >
-              Contact Us
-            </a>
+              className="mobileMd:text-xs  tablet:text-lg font-['DM Sans'] text-white desktopMd:text-2xl not-italic leading-[57px] font-bold"
+            >Contact Us</a>
           </div>
           </nav>
           </div>
         </div>
-        <div className="bg-[#c1ebeb] h-[1000px] w-screen">
-          <div className="text-center pt-[84px] px-40">
-            <span className="font-salsa text-[48px] text-[#1A4654] font-semibold">
+        <div className="bg-[#c1ebeb] desktopMd:h-[900px] w-screen mobileMd:h-screen laptopMd:h-[850px] ">
+          <div className="mobileMd:px-4 mobileMd:pt-5 tablet:px-10 tablet:pt-16 laptopMd:px-28 text-center desktopMd:pt-[84px] desktopMd:px-40">
+            <span className="mobileMd:text-xl tablet:text-3xl laptopMd:text-[44px] font-salsa desktopMd:text-[48px] text-[#1A4654] font-semibold">
               Thoughts Bin
             </span>
-            <p className="pt-10 leading-loose ">
+            <p className="font-['DM Sans']  mobileMd:pt-7 mobileMd:text-[9px] tablet:pt-8 tablet:text-[12px] laptopMd:pt-10 desktopMd:pt-12 desktopMd:leading-loose desktopMd:text-[14px]  ">
               A place to calm your mind. A bin to share your thoughts both good
               and bad freely. It is a place that provides you with the
               fundamental right of expression. All we ask in return is that you
               respect others emotions.
             </p>
-            <p className="pt-10 leading-loose ">
+            <p className="mobileMd:pt-2 desktopMd:pt-10 desktopMd:leading-loose mobileMd:text-[9px] tablet:pt-8 tablet:text-[12px] desktopMd:text-[14px]  ">
               {" "}
               Here what is said matters more than who said. A place where every
               one is equal, a human just expressing. We hope this platform
@@ -64,21 +56,21 @@ const Thoughtbin = () => {
               place were people don't judge just read. There are no celebrities
               here only thoughts floating around.{" "}
             </p>
-            <p className="pt-12 leading-loose ">"Words are a beautiful way to express,</p>
-            <p className="leading-loose "> Thoughts are the soul of what's written,</p>
-            <p className="leading-loose "> What is expressed trough writing is ones emotions,</p>
-            <p className="leading-loose ">Sometimes strong sometimes harsh,</p>
-            <p className="leading-loose "> Sometimes in love other times with a broken heart,</p>
-            <p className="leading-loose "> Lets reduce the anger mend the broken heart and cherish happiness,</p>
-            <p className="leading-loose "> As Sometimes some thoughts live beyond a simple human life"</p>
+            <p className="mobileMd:pt-4 mobileMd:leading-4 desktopMd:pt-12 desktopMd:leading-loose mobileMd:text-[11px] tablet:pt-8 tablet:text-[12px] tablet:leading-7 desktopMd:text-[14px] ">"Words are a beautiful way to express,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7 "> Thoughts are the soul of what's written,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7"> What is expressed trough writing is ones emotions,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7">Sometimes strong sometimes harsh,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7"> Sometimes in love other times with a broken heart,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7"> Lets reduce the anger mend the broken heart and cherish happiness,</p>
+            <p className="desktopMd:leading-loose desktopMd:text-[14px]  mobileMd:leading-4 mobileMd:text-[9px]  tablet:text-[12px] tablet:leading-7"> As Sometimes some thoughts live beyond a simple human life"</p>
           </div>
-          <div className="flex flex-col items-end pr-10 pt-12">
-            <div className="pr-14">
-            <img src={qr}/>
+          <div className="flex flex-col mobileMd:items-center mobileMd:pr-0 tablet:items-end tablet:pr-16 desktopMd:items-end desktopMd:pr-16 desktopMd:pt-8">
+            <div className="mobileMd:pr-0 tablet:pr-7 desktopMd:pr-9 ">
+            <img className="mobileMd:w-[100px] tablet:w-[120px] desktopMd:w-[150px]" src={qr}/>
             </div>
-            <div className="w-[280px] h-[72px] bg-[#12B7B7] mt-5 rounded-sm border-0 ">
+            <div className="mobileMd:w-[120px] mobileMd:h-10 mobileMd:mt-2 tablet:w-[190px] tablet:h-12 desktopMd:w-[230px] desktopMd:h-[65px] bg-[#12B7B7] desktopMd:mt-5 rounded-sm border-0 ">
             <button
-              className=" text-[20px] text-center py-5 text-white font-bold leading-7 "> Get Started 
+              className="mobileMd:text-[15px] mobileMd:py-2 tablet:text-[19px] desktopMd:text-[20px] text-center desktopMd:py-5 text-white font-bold desktopMd:leading-7 "> Get Started 
             </button>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        'salsa':['Salsa', 'cursive'],
-      }
+      fontFamily: {
+        salsa: ["Salsa", "cursive"],
+      },
+      screens: {
+        mobileMd: "375px",
+        mobileLg: "425px",
+        tablet: "768px",
+        laptopSm: "800px",
+        iPadAir: "820px",
+        laptopMd: "1024px",
+        laptopLg: "1280px",
+        desktopSm: "1366px",
+        desktopMd: "1440px",
+        desktopLg: "1920px",
+      },
     },
   },
   plugins: [],
-}
+};
